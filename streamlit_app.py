c5489701741060af09147973bd0b2b65b08ccf95
sfc-gh-craghavendra <101361470+sfc-gh-craghavendra@users.noreply.github.com>
2022-04-21 15:33:26 -0400

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -85,4 +85,23 @@ chart_data = pd.DataFrame(
 st.line_chart(chart_data)
 
 ##### SelectBox ######
+###
 
+st.header('st.selectbox')
+
+option = st.selectbox(
+     'What is your favorite color?',
+     ('Blue', 'Red', 'Green'))
+
+st.write('Your favorite color is ', option)
+
+##### Multiselect #######
+
+st.header('st.multiselect')
+
+options = st.multiselect(
+     'What are your favorite colors',
+     ['Green', 'Yellow', 'Red', 'Blue'],
+     ['Yellow', 'Red'])
+
+st.write('You selected:', options)
