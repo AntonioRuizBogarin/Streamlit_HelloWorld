e6c9fc075753992bfbd74075d40a00f1c9b7dbf6
sfc-gh-craghavendra <101361470+sfc-gh-craghavendra@users.noreply.github.com>
2022-04-22 11:56:23 -0400

    Add files via upload

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import altair as alt
 import pandas as pd
 import streamlit as st
 from datetime import time, datetime
+from streamlit_pandas_profiling import st_profile_report
 
 st.header('st.write')
 
@@ -124,3 +125,13 @@ if coffee:
 
 if cola:
      st.write("Here you go 🥤")
+
+
+### Installing Components ############
+
+st.header('`streamlit_pandas_profiling`')
+
+df = pd.read_csv('https://raw.githubusercontent.com/dataprofessor/data/master/penguins_cleaned.csv')
+
+pr = df.profile_report()
+st_profile_report(pr)
